a93b46ff984005970f776284c227412c5949d6d9
殷兴平 <446160375@qq.com>
2020-04-21 19:08:50 +0800

    作业

--- week02/NOTE.md
+++ week02/NOTE.md
@@ -37,3 +37,85 @@ js类型
 - Null
 - Undefind
 - Symbol
+
+***
+## Atom Identifier & Literal
+***
+* WhiteSpace
+  * \<TAB>：\t
+  * \<VT>： \v
+  * \<FF>：\f
+  * \<SP>：\s
+  * \<NBSP>：NO-BREAK SPACE
+  * \<ZWNBSP>：ZERO WIDTH NO-BREAK SPACE
+  * <USP>
+* LineTerminator
+  * \<LF>：\n
+  * \<CR>：\r
+  * \<LS>
+  * <PS>
+* Comment
+  * // comment
+  * /* comment */
+* CommonToken
+  * IdentifierName
+  * Punctuator
+  * Template
+  * NumericLiteral
+  * StringLiteral
+
+***
+### 图灵完备性
+***
+- 命令式——图灵机
+
+- goto
+
+- if和while
+
+- 声明式——lambda
+
+- 递归
+
+***
+### 动态与静态
+*** 
+- 动态
+
+  - 在用户的设备/在线服务器上
+
+  - 产品实际运行时
+
+  - Runtime
+
+- 静态
+
+  - 在程序员的设备上
+
+  - 产品开发时
+
+  - Compiletime
+
+***
+## 一般命令式编程语言构成
+***
+* Atom
+  * Identifier
+  * Literal
+* Expression
+  * Atom
+  * Operator
+  * Punctuator
+* Statement
+  * Expression
+  * Keyword
+  * Punctuator
+* Structure
+  * Function
+  * Class
+  * Process
+  * Namespace
+* Program
+  * Mould
+  * Package
+  * Library
--- week02/test.js
+++ week02/test.js
@@ -8,37 +8,12 @@ var reg4 = /[\d\D]*/;
 var reg5 = /[\w\W]*/;
 
 // 将字符串格式化为UTF8编码的字节
-var writeUTF = function (str, isGetBytes) {
-    var back = [];
-    var byteSize = 0;
-    for (var i = 0; i < str.length; i++) {
-        var code = str.charCodeAt(i);
-        if (0x00 <= code && code <= 0x7f) {
-              byteSize += 1;
-              back.push(code);
-        } else if (0x80 <= code && code <= 0x7ff) {
-              byteSize += 2;
-              back.push((192 | (31 & (code >> 6))));
-              back.push((128 | (63 & code)))
-        } else if ((0x800 <= code && code <= 0xd7ff) 
-                || (0xe000 <= code && code <= 0xffff)) {
-              byteSize += 3;
-              back.push((224 | (15 & (code >> 12))));
-              back.push((128 | (63 & (code >> 6))));
-              back.push((128 | (63 & code)))
-        }
+function utf8Encoding(str) {
+     let result = ''
+     for (let s of str) {
+          result += `\\u${s.charCodeAt().toString(16)}`
      }
-     for (i = 0; i < back.length; i++) {
-          back[i] &= 0xff;
-     }
-     if (isGetBytes) {
-          return back
-     }
-     if (byteSize <= 0xff) {
-          return [0, byteSize].concat(back);
-     } else {
-          return [byteSize >> 8, byteSize & 0xff].concat(back);
-      }
+     return result
 }
 
 // 作业太难了 上网参考了答案，也看到了自己的短板，努力补下这块内容
