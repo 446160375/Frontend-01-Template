845a39bfeeaea96689886402fb918332b9c84412
殷兴平 <446160375@qq.com>
2020-05-13 16:49:49 +0800

    作业提交2

---
+++ week05/ToyBrowser/server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('x-Foo', 'bar');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('ok');
+});
+
+server.listen(8089, () => {
+    console.log('服务已启动在8089端口！');
+});
